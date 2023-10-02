f0280e706f033a9dc429b481826da9bee144e5f5
valentino <v.arballo@itecriocuarto.org.ar>
2023-10-02 19:01:47 -0300

    handleItemClick

--- src/components/secondlevel/FolderContents.jsx
+++ src/components/secondlevel/FolderContents.jsx
@@ -2,11 +2,20 @@ import PropTypes from 'prop-types'
 import './FolderContents.css'
 
 function FolderContents ({items, itemBackgorund}) {
+    
+    // manejador de clic que recibe un evento 
+    const handleItemClick = (e) => {
+        // rect va a ser una constante durante el ciclo de vida de la funcion, va a representar el elemento HTML
+        const rect = e.target.getBoundingClientRect();
+        // limite izquierdo del elemento, limite superior del elemento
+        console.log(`left ${rect.right} | top ${rect.bottom}`)
+    }
+
     return(
         <ul>
             {items.map((item) => { // este return va a devolver elementos SecondLevelItem
                 return (
-                    <li key={item.id} className='folderContentsItems' style={{background: itemBackgorund}}>
+                    <li key={item.id} className='folderContentsItems' style={{background: itemBackgorund}} onClick={handleItemClick} >
                         {item.name}
                     </li>
                 )
