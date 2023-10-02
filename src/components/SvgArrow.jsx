63defc26e43cc84141bea7843a4b981d4f81867e
valentino <v.arballo@itecriocuarto.org.ar>
2023-10-01 23:48:06 -0300

    SvgArrow component

---
+++ src/components/SvgArrow.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function SvgArrow({arrowColor}) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width='10' height='5'>
+            <path fill={arrowColor} d='M0 0 L10 0 L5 5 Z' />
+    </svg> 
+  )
+}
+
+SvgArrow.propTypes = {
+    arrowColor: PropTypes.string,
+}
